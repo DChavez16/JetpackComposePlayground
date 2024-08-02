dd56f91771e81d50a1f5a804f543555f654e7ac7
Daniel <Daniel_ChavezT@outlook.com>
2024-08-02 14:22:10 -0600

    Declared NavController and CurrentRoute - Created notesNavController in NotesScreen.kt to handle Compose navigation - Created currentRoute in NotesScreen.kt to observe the current destination route in notesNavController

--- feature/remoteDatabase/src/main/java/com/example/remotedatabase/NotesScreen.kt
+++ feature/remoteDatabase/src/main/java/com/example/remotedatabase/NotesScreen.kt
@@ -1,14 +1,24 @@
 package com.example.remotedatabase
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 
 @Composable
 fun NotesScreen(
     onMenuButtonClick: () -> Unit
 ) {
+
+    // Nav controller and current back stack entry to observe the current route
+    val notesNavController = rememberNavController()
+    // Observes the notesNavControlles BackstackEntry as State
+    val currentBackStackEntry by notesNavController.currentBackStackEntryAsState()
+    // Current route based on the current back stack entry
+    val currentRoute: String? = currentBackStackEntry?.destination?.route
 
     // Stores the current ViewModelStoreOwner
     val viewModelStoreOwner = checkNotNull(LocalViewModelStoreOwner.current)
