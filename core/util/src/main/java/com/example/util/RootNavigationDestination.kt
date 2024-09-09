fdb56a59deb9acf2969631b6a9db6a5e3c2c08f5
Daniel <Daniel_ChavezT@outlook.com>
2024-09-09 14:32:11 -0600

    Updated LazyLayouts NavigationDestination icon - Updated RootNavigationDestination.kt so its itemIcon parameter is a Composable function that returns a nullable ImageVector - Updated RootNavigationDestination.kt LazyLayouts destination's itemIcon to the one defined in the UI Kit

--- core/util/src/main/java/com/example/util/RootNavigationDestination.kt
+++ core/util/src/main/java/com/example/util/RootNavigationDestination.kt
@@ -3,58 +3,65 @@ package com.example.util
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 
 
 // Root destinations enum class
 enum class RootNavigationDestination(
     @StringRes val itemTitle: Int?,
     val itemRouteName: String,
-    val itemIcon: ImageVector?
+    val itemIcon: @Composable () -> ImageVector?
 ) {
+
+    // TODO Change the destination item icons to the declared in the Figma UI Kit
+
     LazyLayouts(
         itemTitle = R.string.lazy_layouts_screen,
         itemRouteName = "lazyLayouts",
-        itemIcon = Icons.Default.Build
+        itemIcon = {
+            ImageVector.vectorResource(R.drawable.responsive_layout)
+        }
     ),
     Animations(
         itemTitle = R.string.animations_screen,
         itemRouteName = "animations",
-        itemIcon = Icons.Default.Build
+        itemIcon = { Icons.Default.Build }
     ),
     DrawScope(
         itemTitle = R.string.draw_scope_screen,
         itemRouteName = "drawScope",
-        itemIcon = Icons.Default.Build
+        itemIcon = { Icons.Default.Build }
     ),
     Themes(
         itemTitle = R.string.themes_screen,
         itemRouteName = "themes",
-        itemIcon = Icons.Default.Build
+        itemIcon = { Icons.Default.Build }
     ),
     LocalDatabase(
         itemTitle = R.string.local_database_screen,
         itemRouteName = "localDatabase",
-        itemIcon = Icons.Default.Build
+        itemIcon = { Icons.Default.Build }
     ),
     RemoteDatabase(
         itemTitle = R.string.remote_database_screen,
         itemRouteName = "remoteDatabase",
-        itemIcon = Icons.Default.Build
+        itemIcon = { Icons.Default.Build }
     ),
     DataPersistence(
         itemTitle = R.string.data_persistence_screen,
         itemRouteName = "dataPersistence",
-        itemIcon = Icons.Default.Build
+        itemIcon = { Icons.Default.Build }
     ),
     PersistentWork(
         itemTitle = R.string.persistent_work_screen,
         itemRouteName = "persistentWork",
-        itemIcon = Icons.Default.Build
+        itemIcon = { Icons.Default.Build }
     ),
     Configuration(
         itemTitle = null,
         itemRouteName = "configuration",
-        itemIcon = null
+        itemIcon = { null }
     )
 }
