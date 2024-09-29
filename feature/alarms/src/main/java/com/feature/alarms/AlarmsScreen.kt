c90563334984e1286453a4ac7095972adfc3566c
Daniel <Daniel_ChavezT@outlook.com>
2024-09-29 12:50:20 -0600

    Alarms screen start - Created AlarmsScreen.kt with the AlarmsScreen Composable function

---
+++ feature/alarms/src/main/java/com/feature/alarms/AlarmsScreen.kt
@@ -0,0 +1,12 @@
+package com.feature.alarms
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun AlarmsScreen(
+    onMenuButtonClick: () -> Unit,
+    // TODO Create and add AlarmsViewModel
+) {
+
+}
