5a1b1f18808b1d9ef392eb99bc464bfce20e70b6
Daniel <Daniel_ChavezT@outlook.com>
2024-10-09 15:47:23 -0600

    Continued AlarmsUIUtil implementation - Completed the implementation of AlarmInvokeTypeSelectors Composable - Added relevant string resources to strings.xml that will be used in AlarmInvokeTypeSelectors

--- feature/alarms/src/main/java/com/feature/alarms/ui/AlarmsUIUtil.kt
+++ feature/alarms/src/main/java/com/feature/alarms/ui/AlarmsUIUtil.kt
@@ -19,6 +19,8 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.rounded.BedtimeOff
+import androidx.compose.material.icons.rounded.Repeat
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,6 +28,7 @@ import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -40,12 +43,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.End
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ui.theme.AppTheme
 import com.feature.alarms.AlarmType
+import com.feature.alarms.AlarmsInvokeType
+import com.feature.alarms.ExactAlarmsInvokeType
+import com.feature.alarms.InexactAlarmsInvokeType
 import com.feature.alarms.R
 import kotlinx.coroutines.launch
 
@@ -57,6 +64,9 @@ internal fun AlarmTypeSelectors(
     onAlarmTypeWakeupTypeChange: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+
+    // TODO Add logs at user interactions
+    // TODO Reduce recompositions
 
     // Coroutine scope for pager animations
     val coroutineScope = rememberCoroutineScope()
@@ -108,7 +118,9 @@ internal fun AlarmTypeSelectors(
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp)
         ) {
             // Elapsed type selector
             HorizontalPager(
@@ -179,58 +191,177 @@ internal fun AlarmTypeSelectors(
 
     // Alert dialog providing info about Alarm Types
     if (showAlertDialog) {
-        BasicAlertDialog(
-            onDismissRequest = { showAlertDialog = false },
-            modifier = Modifier
-                .width(300.dp)
-                .background(
-                    color = MaterialTheme.colorScheme.surfaceContainer,
-                    shape = RoundedCornerShape(16.dp)
-                )
-                .padding(16.dp)
-        ) {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(24.dp)
-            ) {
-                // Title of the Alert Dialog
-                Text(
-                    text = stringResource(R.string.alarms_shared_alarm_type_header),
-                    style = MaterialTheme.typography.titleLarge,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    modifier = Modifier
-                        .align(CenterHorizontally)
-                )
-
-                // Content of the Alert Dialog (Alert types description)
-                // Alarm accuracy description
-                Text(
-                    text = stringResource(R.string.alarms_shared_alarm_type_info),
-                    style = MaterialTheme.typography.labelMedium,
-                    textAlign = TextAlign.Justify,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant
-                )
-
-                // Dismiss Button
-                TextButton(
-                    onClick = { showAlertDialog = false },
-                    colors = ButtonDefaults.textButtonColors().copy(
-                        contentColor = MaterialTheme.colorScheme.onSurfaceVariant
-                    ),
-                    modifier = Modifier.align(End)
-                ) {
-                    Text(
-                        text = stringResource(R.string.alarms_screen_alert_dialog_dismiss_button_label)
-                    )
-                }
-            }
-        }
+        InfoAlertDialog(
+            alertDialogTitle = R.string.alarms_shared_alarm_type_header,
+            alertDialogContent = R.string.alarms_shared_alarm_type_info,
+            onDismissRequest = { showAlertDialog = false }
+        )
     }
 }
 
 
 @Composable
-internal fun AlarmInvokeTypeSelectors() {
+internal fun AlarmInvokeTypeSelectors(
+    isExactAlarm: () -> Boolean,
+    currentAlarmInvokeType: () -> AlarmsInvokeType,
+    onChangeInvokeType: (AlarmsInvokeType) -> Unit,
+    modifier: Modifier = Modifier
+) {
 
+    // TODO Add logs at user interactions
+    // TODO Reduce recompositions
+
+    // State for showing or not the info Alert Dialog
+    var showAlertDialog by remember { mutableStateOf(false) }
+
+    // Alarm InvokeType selectos content
+    Column(
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        modifier = modifier.widthIn(max = 500.dp)
+    ) {
+        // Title and info
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(R.string.alarms_shared_alarm_invoke_type_header),
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+
+            Icon(
+                imageVector = Icons.Outlined.Info,
+                contentDescription = stringResource(R.string.alarms_shared_alarm_invoke_type_info_accesibility),
+                tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                modifier = Modifier.clickable {
+                    showAlertDialog = true
+                }
+            )
+        }
+
+        // Selectors
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp)
+        ) {
+            // setWindow() or setAlarmClock() selector
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                // Selector label
+                Text(
+                    text = stringResource(
+                        // If the alarm is exact use the setAlarmClock() selector
+                        if (isExactAlarm()) {
+                            R.string.alarms_shared_alarm_invoke_type_exact_precise_type
+                        }
+                        // Else use the setWindow() selector
+                        else {
+                            R.string.alarms_shared_alarm_invoke_type_inexact_window
+                        }
+                    ),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+
+                // Selector switch
+                Switch(
+                    checked = isInvokeTypeSelectorSwitchActivated(
+                        isExactAlarm = isExactAlarm,
+                        currentAlarmInvokeType = currentAlarmInvokeType
+                    ),
+                    onCheckedChange = { isSwitchNowChecked ->
+                        // If the alarm is exact
+                        if (isExactAlarm()) {
+                            onChangeInvokeType(
+                                // If the switch is checked, return setAlarmClock()
+                                if (isSwitchNowChecked) ExactAlarmsInvokeType.ALARM_CLOCK
+                                // Else, return setExact()
+                                else ExactAlarmsInvokeType.NORMAL
+                            )
+                        }
+                        // Else
+                        else {
+                            onChangeInvokeType(
+                                // If the switch is checked, return setWindow()
+                                if (isSwitchNowChecked) InexactAlarmsInvokeType.WINDOW
+                                // Else, return set()
+                                else InexactAlarmsInvokeType.NORMAL
+                            )
+                        }
+                    }
+                )
+            }
+
+            // setRepeating() and setAndAllowWhileIdle() selectors
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                val selectedIconModifier = Modifier
+                    .shadow(4.dp, shape = RoundedCornerShape(6.dp))
+                    .background(
+                        color = MaterialTheme.colorScheme.surfaceContainer,
+                        shape = RoundedCornerShape(6.dp)
+                    )
+                    .clickable {
+                        onChangeInvokeType(if (isExactAlarm()) ExactAlarmsInvokeType.NORMAL else InexactAlarmsInvokeType.NORMAL)
+                    }
+                    .padding(8.dp)
+
+                // Repeating selector button
+                Icon(
+                    imageVector = Icons.Rounded.Repeat,
+                    contentDescription = stringResource(
+                        if (isRepeatingAlarmSelected(currentAlarmInvokeType)) {
+                            R.string.alarms_shared_alarm_invoke_type_repeating_accesibility_disable
+                        } else R.string.alarms_shared_alarm_invoke_type_repeating_accesibility_enable
+                    ),
+                    tint = if (isRepeatingAlarmSelected(currentAlarmInvokeType)) {
+                        MaterialTheme.colorScheme.primary
+                    } else MaterialTheme.colorScheme.onBackground.copy(alpha = 0.25f),
+                    modifier = if (isRepeatingAlarmSelected(currentAlarmInvokeType)) selectedIconModifier else Modifier
+                        .clickable {
+                            onChangeInvokeType(if (isExactAlarm()) ExactAlarmsInvokeType.REPEATING else InexactAlarmsInvokeType.REPEATING)
+                        }
+                        .padding(8.dp)
+                )
+
+                Spacer(Modifier.width(20.dp))
+
+                // Allow while idle selector button
+                Icon(
+                    imageVector = Icons.Rounded.BedtimeOff,
+                    contentDescription = stringResource(
+                        if (isAllowWhileIdleAlarmSelected(currentAlarmInvokeType)) {
+                            R.string.alarms_shared_alarm_invoke_type_doze_accesibility_disable
+                        } else R.string.alarms_shared_alarm_invoke_type_doze_accesibility_enable
+                    ),
+                    tint = if (isAllowWhileIdleAlarmSelected(currentAlarmInvokeType)) {
+                        MaterialTheme.colorScheme.primary
+                    } else MaterialTheme.colorScheme.onBackground.copy(alpha = 0.25f),
+                    modifier = if (isAllowWhileIdleAlarmSelected(currentAlarmInvokeType)) selectedIconModifier else Modifier
+                        .clickable {
+                            onChangeInvokeType(if (isExactAlarm()) ExactAlarmsInvokeType.ALLOW_WHILE_IDLE else InexactAlarmsInvokeType.ALLOW_WHILE_IDLE)
+                        }
+                        .padding(8.dp)
+                )
+            }
+        }
+    }
+
+    // Alert dialog providing info about Alarm Invoke Types
+    if (showAlertDialog) {
+        InfoAlertDialog(
+            alertDialogTitle = R.string.alarms_shared_alarm_invoke_type_header,
+            alertDialogContent = R.string.alarms_shared_alarm_invoke_type_info,
+            onDismissRequest = { showAlertDialog = false }
+        )
+    }
 }
 
 
@@ -252,6 +383,84 @@ internal fun AlarmActionButtons() {
 }
 
 
+@Composable
+private fun InfoAlertDialog(
+    alertDialogTitle: Int,
+    alertDialogContent: Int,
+    onDismissRequest: () -> Unit
+) {
+    BasicAlertDialog(
+        onDismissRequest = onDismissRequest,
+        modifier = Modifier
+            .width(300.dp)
+            .background(
+                color = MaterialTheme.colorScheme.surfaceContainer,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .padding(16.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            // Title of the Alert Dialog
+            Text(
+                text = stringResource(alertDialogTitle),
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier
+                    .align(CenterHorizontally)
+            )
+
+            // Content of the Alert Dialog
+            Text(
+                text = stringResource(alertDialogContent),
+                style = MaterialTheme.typography.labelMedium,
+                textAlign = TextAlign.Justify,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+
+            // Dismiss Button
+            TextButton(
+                onClick = onDismissRequest,
+                colors = ButtonDefaults.textButtonColors().copy(
+                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                ),
+                modifier = Modifier.align(End)
+            ) {
+                Text(
+                    text = stringResource(R.string.alarms_screen_alert_dialog_dismiss_button_label)
+                )
+            }
+        }
+    }
+}
+
+
+/*
+Helper methods
+ */
+private fun isInvokeTypeSelectorSwitchActivated(
+    isExactAlarm: () -> Boolean,
+    currentAlarmInvokeType: () -> AlarmsInvokeType
+): Boolean {
+    return if (isExactAlarm()) {
+        currentAlarmInvokeType() == ExactAlarmsInvokeType.ALARM_CLOCK
+    } else {
+        currentAlarmInvokeType() == InexactAlarmsInvokeType.WINDOW
+    }
+}
+
+private fun isRepeatingAlarmSelected(
+    currentAlarmInvokeType: () -> AlarmsInvokeType
+): Boolean =
+    currentAlarmInvokeType() == ExactAlarmsInvokeType.REPEATING || currentAlarmInvokeType() == InexactAlarmsInvokeType.REPEATING
+
+
+private fun isAllowWhileIdleAlarmSelected(
+    currentAlarmInvokeType: () -> AlarmsInvokeType
+): Boolean =
+    currentAlarmInvokeType() == ExactAlarmsInvokeType.ALLOW_WHILE_IDLE || currentAlarmInvokeType() == InexactAlarmsInvokeType.ALLOW_WHILE_IDLE
+
 
 /*
 Previews
@@ -262,7 +471,7 @@ private fun AlarmTypeSelectorsPreview() {
     Column(
         verticalArrangement = Arrangement.spacedBy(32.dp),
         modifier = Modifier
-            .widthIn(max = 350.dp)
+            .widthIn(max = 500.dp)
     ) {
         // Light theme preview
         AppTheme(
@@ -308,20 +517,34 @@ private fun AlarmInvokeTypeSelectorsPreview() {
     Column(
         verticalArrangement = Arrangement.spacedBy(32.dp),
         modifier = Modifier
-            .widthIn(max = 350.dp)
+            .widthIn(max = 500.dp)
     ) {
         // Light theme preview
         AppTheme(
             isDarkTheme = { false }
         ) {
+            var previewAlarmInvokeType by remember { mutableStateOf(ExactAlarmsInvokeType.NORMAL) }
 
+            AlarmInvokeTypeSelectors(
+                isExactAlarm = { true },
+                currentAlarmInvokeType = { previewAlarmInvokeType },
+                onChangeInvokeType = { previewAlarmInvokeType = it as ExactAlarmsInvokeType },
+                modifier = Modifier.background(MaterialTheme.colorScheme.background)
+            )
         }
 
         // Dark theme preview
         AppTheme(
             isDarkTheme = { true }
         ) {
+            var previewAlarmInvokeType by remember { mutableStateOf(InexactAlarmsInvokeType.NORMAL) }
 
+            AlarmInvokeTypeSelectors(
+                isExactAlarm = { false },
+                currentAlarmInvokeType = { previewAlarmInvokeType },
+                onChangeInvokeType = { previewAlarmInvokeType = it as InexactAlarmsInvokeType },
+                modifier = Modifier.background(MaterialTheme.colorScheme.background)
+            )
         }
     }
 }
