a6f2f52d4e85f2ce413181f01b9ab10a17103607
Daniel <Daniel_ChavezT@outlook.com>
2024-11-12 14:13:28 -0600

    Widgets feature started - Widgets feature module created - Widgets feature's settings.gradle.kts starting configuration

---
+++ feature/widgets/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    // Feature
+    alias(libs.plugins.playground.feature)
+    // Library Compose
+    alias(libs.plugins.playground.library.compose)
+}
+
+android {
+    namespace = "com.feature.widgets"
+}
+
+dependencies {
+    // Dependencies
+    implementation(projects.core.ui)
+
+    // Testing dependencies
+    testImplementation(projects.core.testing)
+    testImplementation(libs.junit)
+
+    // Android testing
+    androidTestImplementation(projects.core.testing)
+    androidTestImplementation(libs.bundles.androidTest)
+}
