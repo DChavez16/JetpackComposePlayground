481b84689efbdfb01481e37aba39f07663a96c34
Daniel <Daniel_ChavezT@outlook.com>
2024-11-14 15:57:55 -0600

    Started Widgets implementation - Created PlaygroundWidgetReceivers.kt where the receivers for both app Widgets are created - Added the receivers to the feature's AndroidManifest.xml - Created IndividualNoteWidget.kt and AllNotesWidget.kt where the Widgets UI will be defined - Created the Widgets info for 'Individual note' (individual_note_widget_info.xml) and 'All notes' (all_notes_widget_info.xml) - Created a strings.xml resource file to hold the Widget's string resources

---
+++ feature/widgets/src/main/java/com/feature/widgets/receiver/PlaygroundWidgetReceivers.kt
@@ -0,0 +1,16 @@
+package com.feature.widgets.receiver
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+
+// Receiver for the 'IndividualNoteWidget'
+class IndividualNoteReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = TODO("Call UI from the 'IndividualNoteWidget'")
+}
+
+
+// Receiver for the 'AllNotesWidget'
+class AllNotesReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = TODO("Call UI from the 'AllNotesWidget'")
+}
