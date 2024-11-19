4defe542a8feff625be118c8b47b3b772a2466c2
Daniel <Daniel_ChavezT@outlook.com>
2024-11-19 13:46:14 -0600

    Continued Widgets implementation - IndividualNoteWidget (2024.11.19) - Modified IndividualNoteWidget.kt to work around the Widget Preferences - Modified PlaygroundWidgetReceivers.kt to work around the Widget Preferences

--- feature/widgets/src/main/java/com/feature/widgets/receiver/PlaygroundWidgetReceivers.kt
+++ feature/widgets/src/main/java/com/feature/widgets/receiver/PlaygroundWidgetReceivers.kt
@@ -1,14 +1,103 @@
 package com.feature.widgets.receiver
 
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetManager
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.state.getAppWidgetState
+import androidx.glance.appwidget.state.updateAppWidgetState
+import androidx.glance.state.PreferencesGlanceStateDefinition
+import com.example.model.Note
+import com.example.notes.RemoteNoteRepository
 import com.feature.widgets.ui.AllNotesWidget
 import com.feature.widgets.ui.IndividualNoteWidget
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+
+//https://stackoverflow.com/questions/71381707/how-to-create-an-instance-of-room-dao-or-repository-or-viewmodel-in-glanceappwid/72168589#72168589
+//&
+//https://stackoverflow.com/questions/75511282/display-data-from-database-room-in-widget-glance-using-jetpack-compose
 
 // Receiver for the 'IndividualNoteWidget'
+@AndroidEntryPoint
 class IndividualNoteReceiver : GlanceAppWidgetReceiver() {
+
+    // IndividualNoteWidget instance
     override val glanceAppWidget: GlanceAppWidget = IndividualNoteWidget()
+
+    // Preferences keys
+    companion object {
+        val PINNED_NOTE_ID = intPreferencesKey("selected_note_id")
+        val PINNED_NOTE_TITLE = stringPreferencesKey("selected_note_title")
+    }
+
+    // Inject NoteRepository
+    @Inject
+    lateinit var noteRepository: RemoteNoteRepository
+
+    // Define a coroutine scope
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    // Callback when the Widget is updated
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+        // Retreive pinned note
+        retreivePinnedNote(context)
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+
+        // Retreive pinned note
+        retreivePinnedNote(context)
+    }
+
+    // Function to retreive the pinned note
+    fun retreivePinnedNote(context: Context) {
+        coroutineScope.launch {
+            // Get the id of the widget
+            val glanceId =
+                GlanceAppWidgetManager(context).getGlanceIds(glanceAppWidget.javaClass).first()
+            Log.d("IndividualNoteReceiver", "Retreiving pinned note for widget with id: $glanceId")
+
+            glanceId.let {
+                // Get the pinned note id
+                val prefs = getAppWidgetState(context, PreferencesGlanceStateDefinition, it)
+                val pinnedNoteId = prefs[PINNED_NOTE_ID]
+                Log.d("IndividualNoteReceiver", "Pinned note id: $pinnedNoteId")
+
+                // Obtain the note with the given id from the repository
+                val note: Note? =
+                    noteRepository.getNotes().find { it.id == (pinnedNoteId?.toLong() ?: -1) }
+                Log.d("IndividualNoteReceiver", "Retreived note id: ${note?.id ?: "null"}")
+
+                // Sets the note title as the PINNED_NOTE_TITLE in the Widget's preferences
+                updateAppWidgetState(context, PreferencesGlanceStateDefinition, it) { prefs ->
+                    prefs.toMutablePreferences().apply {
+                        this[PINNED_NOTE_TITLE] = note?.title ?: ""
+                        Log.d(
+                            "IndividualNoteReceiver",
+                            "PINNED_NOTE_TITLE preferences changed to ${note?.title ?: ""}"
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
 
--- feature/widgets/src/main/java/com/feature/widgets/ui/IndividualNoteWidget.kt
+++ feature/widgets/src/main/java/com/feature/widgets/ui/IndividualNoteWidget.kt
@@ -3,26 +3,29 @@ package com.feature.widgets.ui
 import android.content.Context
 import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
+import androidx.datastore.preferences.core.Preferences
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.appwidget.state.updateAppWidgetState
+import androidx.glance.appwidget.updateAll
 import androidx.glance.background
 import androidx.glance.color.ColorProvider
+import androidx.glance.currentState
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.state.PreferencesGlanceStateDefinition
 import androidx.glance.text.Text
 import androidx.glance.text.TextAlign
 import androidx.glance.text.TextStyle
-import com.example.model.Note
-import com.feature.widgets.HiltEntryPoint.WidgetsEntryPoint
-import dagger.hilt.android.EntryPointAccessors
-import kotlinx.coroutines.flow.MutableStateFlow
+import com.feature.widgets.receiver.IndividualNoteReceiver
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 private const val TAG = "IndividualNoteWidget"
 
@@ -32,23 +35,39 @@ class IndividualNoteWidget : GlanceAppWidget() {
 
         Log.i(TAG, "AllNotesWidget started")
 
-        val notesEntryPoint = EntryPointAccessors.fromApplication(
-            context.applicationContext, WidgetsEntryPoint::class.java,
-        )
-
         provideContent {
             val coroutineScope = rememberCoroutineScope()
-            val noteRepository = notesEntryPoint.getNoteRepository()
-            var notesUiState = remember {
-                MutableStateFlow<IndividualNoteWidgetUiState>(IndividualNoteWidgetUiState.Loading)
-            }
 
-            // TODO Add in note repository a way to store a single note id using DataStore
-            // TODO OR
-            // TODO Store it in the widgets preferences
+            val prefs = currentState<Preferences>()
+            val noteTitle = prefs[IndividualNoteReceiver.PINNED_NOTE_TITLE]
+            Log.d(TAG, "Note title: $noteTitle")
 
             IndividualNoteWidgetContent(
-                notesUiState = notesUiState.collectAsState().value,
+                noteTitle = noteTitle,
+                updatePinnedNote = {
+                    coroutineScope.launch(Dispatchers.IO) {
+                        updateAppWidgetState(
+                            context,
+                            PreferencesGlanceStateDefinition,
+                            id
+                        ) { prefs ->
+                            prefs.toMutablePreferences().apply {
+                                Log.d(
+                                    TAG,
+                                    "PINNED_NOTE_ID preferences before changes ${this[IndividualNoteReceiver.PINNED_NOTE_ID]}"
+                                )
+                                this[IndividualNoteReceiver.PINNED_NOTE_ID] =
+                                    this[IndividualNoteReceiver.PINNED_NOTE_ID]?.plus(1) ?: -1
+                                Log.d(
+                                    TAG,
+                                    "PINNED_NOTE_ID preferences changed to ${this[IndividualNoteReceiver.PINNED_NOTE_ID]}"
+                                )
+                            }
+                        }
+
+                        IndividualNoteWidget().updateAll(context)
+                    }
+                }
             )
         }
     }
@@ -57,51 +76,30 @@ class IndividualNoteWidget : GlanceAppWidget() {
 
 @Composable
 private fun IndividualNoteWidgetContent(
-    notesUiState: IndividualNoteWidgetUiState
+    noteTitle: String?,
+    updatePinnedNote: () -> Unit
 ) {
-    when (notesUiState) {
-        is IndividualNoteWidgetUiState.Loading -> WidgetLoadingContent()
-        is IndividualNoteWidgetUiState.Error -> WidgetErrorContent(
-            errorMessage = notesUiState.errorMessage
-        )
-
-        is IndividualNoteWidgetUiState.Success -> WidgetSuccessContent(
-            note = notesUiState.notes
-        )
-    }
-}
-
-@Composable
-private fun WidgetLoadingContent(
-
-) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = GlanceModifier
-            .background(color = Color.Yellow)
-            .fillMaxSize()
-    ) {
-        Text(
-            text = "Loading...",
-            style = TextStyle(
-                color = ColorProvider(day = Color.Black, night = Color.Black)
-            )
-        )
+    when (noteTitle) {
+        "" -> WidgetErrorContent("No pinned note", updatePinnedNote)
+        null -> WidgetErrorContent("Connection error", updatePinnedNote)
+        else -> WidgetSuccessContent(noteTitle, updatePinnedNote)
     }
 }
 
 @Composable
 private fun WidgetErrorContent(
-    errorMessage: String
+    errorMessage: String,
+    onUpdatePinnedNoteClick: () -> Unit
 ) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = GlanceModifier
             .background(color = Color.Red)
             .fillMaxSize()
+            .clickable(onUpdatePinnedNoteClick)
     ) {
         Text(
-            text = "Error!!\n$errorMessage",
+            text = errorMessage,
             style = TextStyle(
                 color = ColorProvider(day = Color.White, night = Color.White),
                 textAlign = TextAlign.Center
@@ -111,26 +109,22 @@ private fun WidgetErrorContent(
 }
 
 @Composable
-fun WidgetSuccessContent(note: Note) {
+fun WidgetSuccessContent(
+    noteTitle: String,
+    onUpdatePinnedNoteClick: () -> Unit
+) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = GlanceModifier
             .background(color = Color.Green)
             .fillMaxSize()
+            .clickable(onUpdatePinnedNoteClick)
     ) {
         Text(
-            text = "Success!!\nNote title: ${note.title}",
+            text = "Success!!\nNote title: $noteTitle",
             style = TextStyle(
                 color = ColorProvider(day = Color.Black, night = Color.Black)
             )
         )
     }
-}
-
-
-// Helper UI State
-private sealed interface IndividualNoteWidgetUiState {
-    data class Success(val notes: Note) : IndividualNoteWidgetUiState
-    data class Error(val errorMessage: String) : IndividualNoteWidgetUiState
-    data object Loading : IndividualNoteWidgetUiState
 }
