6e88c47c9f3497ed8ad0c8fed4cace8def48ec61
Daniel <Daniel_ChavezT@outlook.com>
2025-03-13 19:34:46 -0600

    Continued implementation of opening the pinned note in the app (2025.03.13) - Modified RootNavHost.kt remote database navigation to let the use of deep links

--- app/mobile/src/main/java/com/example/mobile/util/RootNavHost.kt
+++ app/mobile/src/main/java/com/example/mobile/util/RootNavHost.kt
@@ -1,9 +1,13 @@
 package com.example.mobile.util
 
+import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.core.net.toUri
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navDeepLink
 import com.example.animations.AnimationScreen
 import com.example.configuration.ConfigurationScreen
 import com.example.datapersistence.DataPersistenceScreen
@@ -16,6 +20,7 @@ import com.example.themes.ThemeScreen
 import com.example.util.RootNavigationDestination
 import com.feature.alarms.AlarmsScreen
 
+const val URI = "https://www.compose-playground.com"
 
 // Root Navigation Composable function
 @Composable
@@ -75,11 +80,41 @@ internal fun RootNavHost(
 
         // Remote database destination
         composable(
-            route = RootNavigationDestination.RemoteDatabase.itemRouteName
-        ) {
+            route = RootNavigationDestination.RemoteDatabase.itemRouteName,
+            deepLinks = listOf(
+                navDeepLink {
+                    uriPattern = "$URI/notes/{notesPath}/{noteId}"
+                }
+            )
+        ) { backStackEntry ->
+
+            // Obtain the parameters from the backStackEntry
+            val notesPath = backStackEntry.arguments?.getString("notesPath")
+            val noteId = backStackEntry.arguments?.getLong("noteId")
+
+            // Notes Screen nav controller
+            val notesScreenNavController = rememberNavController()
+
             NotesScreen(
+                notesNavController = notesScreenNavController,
                 onMenuButtonClick = onMenuButtonClick
             )
+
+            // If the deeplink paramethers are not null, navigate notesScreenNavController to the desired Uri
+            if(notesPath != null) {
+                Log.d("RootNavHost", "Path obtained from the backstack entry: $notesPath. with note id: ${noteId ?: "null"}")
+
+                val deepLinkUri = when(notesPath) {
+                    "notesList" -> { "$URI/notes/notesList/$noteId".toUri() }
+                    "newNote" -> { "$URI/notes/newNote".toUri() }
+                    else -> {
+                        // Redirect to the notes list
+                        "$URI/notes/notesList".toUri()
+                    }
+                }
+
+                notesScreenNavController.navigate(deepLinkUri)
+            }
         }
 
         // Data Persistence destination
