3c146bf317a8cf744d3d9a9058f3485b694afdf6
Daniel <Daniel_ChavezT@outlook.com>
2025-05-02 20:33:15 -0600

    Navigation re factorization (2025.05.02) - Completed LocalDatabaseNavigation.kt changes to follow the new nested navigation approach   * Added ProductListScreen as the "Product List" destination   * Added AddProductScreen in normal mode as the "Add Product" destination   * Added AddProductScreen in edit mode as the "Edit Product" destination

--- feature/localDatabase/src/main/java/com/example/room/util/LocalDatabaseNavigation.kt
+++ feature/localDatabase/src/main/java/com/example/room/util/LocalDatabaseNavigation.kt
@@ -2,22 +2,23 @@ package com.example.room.util
 
 import android.util.Log
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModelStoreOwner
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.example.model.isValidForSave
 import com.example.room.R
 import com.example.room.ui.AddProductScreen
 import com.example.room.ui.ProductListScreen
 import com.example.room.ui.ProductsViewModel
+
+
+private const val LOG_TAG = "LocalDatabaseNavHost"
 
 // Local database destinations enum class
 internal enum class LocalDatabaseDestinations(
@@ -38,67 +39,7 @@ internal enum class LocalDatabaseDestinations(
     )
 }
 
-
-// Local Database NavHost Composable function
-@Composable
-internal fun LocalDatabaseNavHost(
-    navController: NavHostController,
-    viewModelStoreOwner: () -> ViewModelStoreOwner,
-    navigateToProduct: (Long) -> Unit,
-    onAddProductClick: () -> Unit,
-    onUpdateProductClick: () -> Unit,
-    innerPadding: () -> PaddingValues
-) {
-    NavHost(
-        navController = navController,
-        startDestination = LocalDatabaseDestinations.ProductsList.screenRouteName,
-        modifier = Modifier.padding(innerPadding())
-    ) {
-        // Products List destination
-        composable(
-            route = LocalDatabaseDestinations.ProductsList.screenRouteName
-        ) {
-
-            Log.i("ProductListScreen", "ProductListScreen created")
-
-            ProductListScreen(
-                navigateToProduct = navigateToProduct,
-                // Sets as a parameter the viewModel instance binded to viewModelStoreOwner
-                productsViewModel = hiltViewModel<ProductsViewModel>(viewModelStoreOwner())
-            )
-        }
-
-        // Edit Product destination
-        composable(
-            route = LocalDatabaseDestinations.EditProduct.screenRouteName
-        ) {
-
-            Log.i("AddProductScreen", "Screen created in edit mode")
-
-            AddProductScreen(
-                onSaveProduct = onUpdateProductClick,
-                // Sets as a parameter the viewModel instance binded to viewModelStoreOwner
-                productsViewModel = hiltViewModel<ProductsViewModel>(viewModelStoreOwner())
-            )
-        }
-
-        // Add Product destination
-        composable(
-            route = LocalDatabaseDestinations.AddProduct.screenRouteName
-        ) {
-
-            Log.i("AddProductScreen", "Screen created in add mode")
-
-            AddProductScreen(
-                onSaveProduct = onAddProductClick,
-                // Sets as a parameter the viewModel instance binded to viewModelStoreOwner
-                productsViewModel = hiltViewModel<ProductsViewModel>(viewModelStoreOwner())
-            )
-        }
-    }
-}
-
-
+// Local database navigation graph
 fun NavGraphBuilder.localDatabaseGraph(
     navController: NavHostController,
     graphRoute: String,
@@ -113,30 +54,107 @@ fun NavGraphBuilder.localDatabaseGraph(
         composable(
             route = LocalDatabaseDestinations.ProductsList.screenRouteName
         ) {
+            Log.i(LOG_TAG, "ProductListScreen created")
+
             // Creates viewModel using the parent entry
             val parentEntry = remember { navController.getBackStackEntry(graphRoute) }
-            val notesViewModel = hiltViewModel<ProductsViewModel>(parentEntry)
+            val productsViewModel = hiltViewModel<ProductsViewModel>(parentEntry)
 
+            // Products list uiState
+            val productsUiState by productsViewModel.productsUiState.collectAsState()
+
+            ProductListScreen(
+                productsUiState = { productsUiState },
+                onAddProductClick = {
+                    // Clears the current product
+                    productsViewModel.setNewProduct()
+
+                    // Navigates to the "Add product" screen
+                    navController.navigate(LocalDatabaseDestinations.AddProduct.screenRouteName)
+                },
+                navigateToProduct = { selectedProductId ->
+                    // Changes the current product based in the ID
+                    productsViewModel.changeCurrentProductByID(selectedProductId)
+
+                    // Navigates to the "Add Product" screen in edit mode
+                    navController.navigate(LocalDatabaseDestinations.EditProduct.screenRouteName)
+                },
+                onMenuButtonClick = onMenuButtonClick
+            )
         }
 
         // Add Product destination
         composable(
             route = LocalDatabaseDestinations.AddProduct.screenRouteName
         ) {
+            Log.i(LOG_TAG, "AddProductScreen created in add mode")
+
             // Creates viewModel using the parent entry
             val parentEntry = remember { navController.getBackStackEntry(graphRoute) }
-            val notesViewModel = hiltViewModel<ProductsViewModel>(parentEntry)
+            val productsViewModel = hiltViewModel<ProductsViewModel>(parentEntry)
 
+            // Current product state
+            val currentProduct by productsViewModel.currentProduct.collectAsState()
+
+            // Top App Bar title
+            val topAppBarTitle = stringResource(LocalDatabaseDestinations.AddProduct.screenTitle)
+
+            AddProductScreen(
+                currentProduct = { currentProduct },
+                onSaveProduct = {
+                    // Insert a new product in the database
+                    productsViewModel.insertProduct()
+
+                    // Returns to the previous screen
+                    navController.popBackStack()
+                },
+                onProductNameChange = productsViewModel::updateProductName,
+                onProductQuantityChange = productsViewModel::updateProductQuantity,
+                onProductDescriptionChange = productsViewModel::updateProductDescription,
+                onProductAvailabilityChange = productsViewModel::updateProductAvailability,
+                clearProductFields = productsViewModel::clearProductFields,
+                isSaveButtonEnabled = { currentProduct.isValidForSave() },
+                topAppBarTitle = topAppBarTitle,
+                onBackButtonPressed = navController::popBackStack
+            )
         }
 
         // Edit Product destination
         composable(
             route = LocalDatabaseDestinations.EditProduct.screenRouteName
         ) {
+            Log.i(LOG_TAG, "AddProductScreen created in edit mode")
+
             // Creates viewModel using the parent entry
             val parentEntry = remember { navController.getBackStackEntry(graphRoute) }
-            val notesViewModel = hiltViewModel<ProductsViewModel>(parentEntry)
+            val productsViewModel = hiltViewModel<ProductsViewModel>(parentEntry)
 
+            // Current product state
+            val currentProduct by productsViewModel.currentProduct.collectAsState()
+
+            // Top App Bar title
+            val topAppBarTitle by productsViewModel.currentProductName.collectAsState()
+
+            AddProductScreen(
+                currentProduct = { currentProduct },
+                onSaveProduct = {
+                    // Update the current product in the database
+                    productsViewModel.updateProduct()
+
+                    // Returns to the previous screen
+                    navController.popBackStack()
+                },
+                onProductNameChange = productsViewModel::updateProductName,
+                onProductQuantityChange = productsViewModel::updateProductQuantity,
+                onProductDescriptionChange = productsViewModel::updateProductDescription,
+                onProductAvailabilityChange = productsViewModel::updateProductAvailability,
+                clearProductFields = productsViewModel::clearProductFields,
+                isSaveButtonEnabled = { currentProduct.isValidForSave() },
+                topAppBarTitle = topAppBarTitle,
+                onBackButtonPressed = navController::popBackStack,
+                showDeletionActionButton = true,
+                onDeleteProduct = productsViewModel::deleteProduct
+            )
         }
     }
 }
